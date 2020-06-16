3402d609ec21d6e3896913aac430a7329ab53586
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-06-16 21:01:48 +0530

    Use express to check heroku

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const players = {};
 const games = {};
 let food = {};
 
-const httpServer = http.createServer((req, res) => {
-    console.log('HTTP request');
-})
+const httpServer = http.createServer(app);
 
 const ws = new WebSocket({ "httpServer": httpServer });
 
